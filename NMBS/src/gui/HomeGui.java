07e2eea17542f89e8c24adbaffa66db92142942f
jonas de gauquier <jonasdg@hotmail.be>
2016-10-27 19:16:05 +0200

    verwijderen onnodige imports

--- NMBS/src/gui/HomeGui.java
+++ NMBS/src/gui/HomeGui.java
@@ -1,19 +1,19 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
 import java.io.File;
-import java.util.ResourceBundle;
-
-import javax.swing.BorderFactory;
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class HomeGui extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2564170957766548127L;
+
 	private JPanel jPanel, jPanel2;
 
 	ImageIcon photo;
@@ -38,7 +38,6 @@ public class HomeGui extends JPanel {
 		add(jPanel);
 		
 		GroupLayout layout = new GroupLayout(jPanel);
-		jPanel.setLayout(layout);
-		
+		jPanel.setLayout(layout);		
 	}
 }
