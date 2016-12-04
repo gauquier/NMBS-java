93f758bd97fa058596ac97b1d83c573f75b011db
Robincck <Robin.couck@student.ehb.be>
2016-12-04 15:09:53 +0100

    Aanpassingen aan Login/MedewerkerDAO

--- NMBS/src/dao/MedewerkerDAO.java
+++ NMBS/src/dao/MedewerkerDAO.java
@@ -1,5 +1,11 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 import source.Adres;
 import source.Login;
 import source.Medewerker;
@@ -9,6 +15,11 @@ import source.Rol;
 public class MedewerkerDAO {
 	
 	private static DBA dba = new DBA();
+	
+	private static java.sql.Connection connection;
+	private static Statement command;
+	private static ResultSet data;
+	private static PreparedStatement stmt = null;
 	
 	public static void addMedewerker(Login login, Persoon persoon, Rol rol, Adres adres){
 		int loginId =  0, persoonId = 0;
@@ -23,4 +34,6 @@ public class MedewerkerDAO {
 		dba.addValue(0);
 		dba.commit();
 	}
+	
+	
 }
