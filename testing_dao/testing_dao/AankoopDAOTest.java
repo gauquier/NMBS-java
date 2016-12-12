d084526f462ece001e285403e4a42ec67d186c0c
Aime <ALIENWARE_BEEST@ALIENWARE_BEEST>
2016-12-12 14:05:46 +0100

    Testing

---
+++ testing_dao/testing_dao/AankoopDAOTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import dao.AankoopDAO;
+
+public class AankoopDAOTest {
+	private AankoopDAO aankoopDAO;
+	@Before
+	public void Initialization(){
+		aankoopDAO= new AankoopDAO();
+		
+	} 
+	@Test
+	public void testAddAankoop() {
+		fail("Not yet implemented");
+	}
+
+}
