adb35ac567f2391e46e10e8c326fe0dfeb1d8190
Aime <PEGASUS@PEGASUS>
2016-12-20 23:59:58 +0100

    LoginDAOTest
    	-testOnbestaandeLoginToevoegen
    	-testBestaandeLoginToevoegen
    	-testGetLoginId
    	-testGetLoginIdMetOnbestaandeLogin
    	-testGetLogin
    	-testGetLoginMetOnbestaandeId
    	-testCheckUsername
    	-testCheckUsernameMetOnbestaandeUsername
    	-testGetLoginIdMetUsername
    	-testGetLoginIdMetOnbestaandeUsername

--- testing/dao/LoginDAOTest.java
+++ testing/dao/LoginDAOTest.java
@@ -2,45 +2,240 @@ package dao;
 
 import static org.junit.Assert.*;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import dao.LoginDao;
+import source.Adres;
+import source.Login;
+import source.Medewerker;
 import source.Persoon;
+import source.Rol;
 
 public class LoginDAOTest {
-	
+
 	private Persoon persoon;
-	
+
 	private String username, password, fakeUsername, fakePassword;
-	 private int userId, fakeUserID, rollId, fakeRollId;
-	public void initialize(){
-		
-		
+	private int userId, fakeUserID, rollId, fakeRollId;
+	private Adres adres;
+	private Rol rol;
+	private Medewerker medewerker;
+	private Login login;
+
+	@Before
+	public void initialize() {
+		// adres = adresToevoegen(new Adres("teststraatnaamLoginDAOTest", 170,
+		// "testwoonplaatsLoginDAOTest", 1070, "6"));
+		// rol= rolToevoegen(new Rol(0, "testrolLoginDAOTest"));
+		// this is not needed to test login
+		// but useful for medewerkerdao test
+		login = new Login("testusernameLoginDAOTest", "testpasswordLoginDAOTest");
+
 	}
-	public void testBestaandeLogin(){}
-	public void testOnBestaandeLogin(){}
-	public void testPersoonDieGeenMedewerkerIs(){}
-	public void testOnbestaandeRol(){}
-	public void testGetLoginId(){}
-	public void testGetLoginIDMetOnbestaandeLogin(){}
-	public void testVerkeerdWW(){}
-	public void testGetLogin(){}
-	public void testGetLoginMetOnbestaandeId(){}
-	public void testGetLoginMetUserDieMetVerkeerdLoginInfoIsGekopeld(){}
-	public void testCheckUsername(){}
-	public void testCheckUsernameMetOnbestaandeUsername(){}
-	public void testGetRoll(){}
-	public void testGetRollMetOnbestaandeRoll(){}
-	public void testGetLoginIdMetUsername(){}
-	public void testGetLoginIdMetOnbestaandeUsername(){}
-	public void testGetWachtwoord(){}
-	public void testGetWachtwoordMetOnbestaandeUsername(){}
-	public void testUpdadeWachtwoord(){}
-	public void testUpdateWachtwoordWhere(){}
-	public void testGetActief(){}
-	public void testGetActiefMetOnbestaandeid(){}
-	//public void testGetUserName(){}
+
+	@After
+	public void terminate() {
+		String loginVerwijderen = "DELETE FROM Login WHERE username LIKE '%LoginDAOTest%'";
+		executeQuery(loginVerwijderen, true);
+
+	}
+
+	@Test
+	public void testOnbestaandeLoginToevoegen() {
+		int id = LoginDao.addLogin(login);
+		Login loginVanUitDB = loginOphalen(login);
+		assertEquals(login, loginVanUitDB);
+		assertEquals(loginVanUitDB.getLoginId(), id);
+	}
+
+	@Test(expected = Exception.class)
+	public void testBestaandeLoginToevoegen() {
+		LoginDao.addLogin(login);
+		LoginDao.addLogin(login);
+	}
+
+	@Test
+	public void testGetLoginId() {
+		Login loginVanUitDB = loginToevoegen(login);
+		int id = LoginDao.getLoginId(login);
+		assertEquals(loginVanUitDB.getLoginId(), id);
+	}
+
+	@Test
+	public void testGetLoginIdMetOnbestaandeLogin() {
+		assertEquals(0, LoginDao.getLoginId(login));
+	}
+
+	// @Test
+	// public void testVerkeerdWW() {
+	// er bestaat zo een methode nietè
+	// loginToevoegen(login);
+	// login.setPassword("verkeerdWW");
+	//
+	// }
+	@Test
+	public void testGetLogin() {
+		Login loginVanUitDB = loginToevoegen(login);
+		assertEquals(loginVanUitDB, LoginDao.getLogin(loginVanUitDB.getLoginId())); 
+	}
+	@Test
+	public void testGetLoginMetOnbestaandeId() {
+		int onBestaandeLoginId=91119;
+		assertNull(LoginDao.getLogin(onBestaandeLoginId)); 
+	} 
+	@Test
+	public void testCheckUsername() {
+		Login loginVanUitDB = loginToevoegen(login);
+		LoginDao loginDao= new LoginDao();
+		assertEquals(loginVanUitDB.getLoginId(), loginDao.checkUsername(login.getUsername())); 
+	}
+	@Test
+	public void testCheckUsernameMetOnbestaandeUsername() {
+		LoginDao loginDao= new LoginDao();
+		assertEquals(-1, loginDao.checkUsername(login.getUsername())); 
+	}
+//	ik weet niet wat de bedoeling is van dit methode
+//	public void testGetRoll() {
+//	}
+//
+//	public void testGetRollMetOnbestaandeRoll() {
+//	}
+	@Test
+	public void testGetLoginIdMetUsername() {
+		Login loginVanUitDB = loginToevoegen(login);
+		assertEquals(loginVanUitDB.getLoginId(), LoginDao.getLoginId(login.getUsername())); ;
+	}
+	@Test
+	public void testGetLoginIdMetOnbestaandeUsername() {
+		assertEquals(0, LoginDao.getLoginId(login.getUsername())); 
+	}
+
+	public void testGetWachtwoord() {
+	}
+
+	public void testGetWachtwoordMetOnbestaandeUsername() {
+	}
+
+	public void testUpdadeWachtwoord() {
+	}
+
+	public void testUpdateWachtwoordWhere() {
+	}
+
+	public void testGetActief() {
+	}
+
+	public void testGetActiefMetOnbestaandeid() {
+	}
+
+	public Login loginToevoegen(Login login) {
+		String loginToevoen = "INSERT INTO Login (username, pass) " + "VALUES(?,?)";
+		executeQuery(loginToevoen, true, login.getUsername(), login.getPassword());
+		return loginOphalen(login);
+	}
+
+	public void medewerkerToevoegen() {
+	}
+
+	public void persoonToevoegen() {
+	}
+
+	public Rol rolToevoegen(Rol rol) {
+
+		return null;
+	}
+
+	public Adres adresToevoegen(Adres adres) {
+		String AdresToevoegenQuery = "INSERT INTO Adres (straat, huisnr, woonplaats, postcode, bus) VALUES(?,?,?,?,?)";
+		String AdresZoekenQuery = "SELECT adresId FROM Adres WHERE straat = ?";
+		executeQuery(AdresToevoegenQuery, true, adres.getStraat(), adres.getHuisnr(), adres.getWoonplaats(),
+				adres.getPostcode(), adres.getBus());
+		Map<Integer, Object[]> map = executeQuery(AdresZoekenQuery, false, adres.getStraat());
+		adres.setAdresId((int) map.get(0)[0]);
+		return adres;
+	}
+
+	public Rol rolOphalen(String rol) {
+		return null;
+	}
+
+	public Rol rolToevoegen(String rolNaam) {
+
+		return null;
+	}
+
+	private Map<Integer, Object[]> executeQuery(String query, boolean update, Object... kolomWaarden) {
+		Map<Integer, Object[]> map = new HashMap<Integer, Object[]>();
+		java.sql.Connection connection = null;
+		PreparedStatement stmt = null;
+		ResultSet resultSet = null;
+		try {
+			connection = Connection.getDBConnection();
+			connection.setAutoCommit(false);
+			stmt = connection.prepareStatement(query);
+			for (int i = 0; i < kolomWaarden.length; i++) {
+				stmt.setObject(i + 1, kolomWaarden[i]);
+			}
+			if (update) {
+				stmt.executeUpdate();
+				connection.commit();
+			} else {
+				resultSet = stmt.executeQuery();
+				ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+				int columnCount = resultSetMetaData.getColumnCount();
+				resultSet.last();
+				int size = resultSet.getRow();
+				resultSet.beforeFirst();
+				int counter = 0;
+
+				if (size != 0) {
+					while (resultSet.next()) {
+						Object[] kolommen = new Object[columnCount];
+						for (int i = 1; i <= columnCount; i++) {
+							kolommen[i - 1] = resultSet.getObject(i);
+						}
+						map.put(counter, kolommen);
+						counter++;
+					}
+					;
+
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (resultSet != null)
+					resultSet.close();
+				if (stmt != null)
+					stmt.close();
+				if (connection != null)
+					connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return map;
+	}
+
+	private Login loginOphalen(Login login) {
+		String loginOphalen = "SELECT loginId FROM Login WHERE username LIKE '%LoginDAOTest%'";
+		Map<Integer, Object[]> map = executeQuery(loginOphalen, false);
+		login.setLoginId((int) map.get(0)[0]);
+		return login;
+	}
+	// public void testGetUserName(){}
 	// login is totaal anders in klassendiagram
 	// en deze testen zijn niet meer van toepasing
 	// -Aime
--- testing/dao/MedwerkerDAOTest.java
+++ testing/dao/MedwerkerDAOTest.java
@@ -4,16 +4,177 @@ import static org.junit.Assert.*;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import dao.Connection;
 import dao.MedewerkerDAO;
+import source.Adres;
+import source.Login;
 import source.Medewerker;
+import source.Rol;
 
 public class MedwerkerDAOTest {
+
+//	public void testPersoonDieGeenMedewerkerIs() {
+//	}
+//
+//	public void testOnbestaandeRol() {
+//	}
+//
+//	public void testGetLoginId() {
+//	}
+//
+//	public void testGetLoginIDMetOnbestaandeLogin() {
+//	}
+//
+//	public void testVerkeerdWW() {
+//	}
+//
+//	public void testGetLogin() {
+//	}
+//
+//	public void testGetLoginMetOnbestaandeId() {
+//	}
+//
+//	public void testGetLoginMetUserDieMetVerkeerdLoginInfoIsGekopeld() {
+//	}
+//
+//	public void testCheckUsername() {
+//	}
+//
+//	public void testCheckUsernameMetOnbestaandeUsername() {
+//	}
+//
+//	public void testGetRoll() {
+//	}
+//
+//	public void testGetRollMetOnbestaandeRoll() {
+//	}
+//
+//	public void testGetLoginIdMetUsername() {
+//	}
+//
+//	public void testGetLoginIdMetOnbestaandeUsername() {
+//	}
+//
+//	public void testGetWachtwoord() {
+//	}
+//
+//	public void testGetWachtwoordMetOnbestaandeUsername() {
+//	}
+//
+//	public void testUpdadeWachtwoord() {
+//	}
+//
+//	public void testUpdateWachtwoordWhere() {
+//	}
+//
+//	public void testGetActief() {
+//	}
+//
+//	public void testGetActiefMetOnbestaandeid() {
+//	}
+//
+//	public void loginToevoegen() {
+//	}
+//
+//	public void medewerkerToevoegen() {
+//	}
+//
+//	public void persoonToevoegen() {
+//	}
+//
+//	public Rol rolToevoegen(Rol rol) {
+//
+//		return null;
+//	}
+//
+//	public Adres adresToevoegen(Adres adres) {
+//		String AdresToevoegenQuery = "INSERT INTO Adres (straat, huisnr, woonplaats, postcode, bus) VALUES(?,?,?,?,?)";
+//		String AdresZoekenQuery = "SELECT adresId FROM Adres WHERE straat = ?";
+//		executeQuery(AdresToevoegenQuery, true, adres.getStraat(), adres.getHuisnr(), adres.getWoonplaats(),
+//				adres.getPostcode(), adres.getBus());
+//		Map<Integer, Object[]> map = executeQuery(AdresZoekenQuery, false, adres.getStraat());
+//		adres.setAdresId((int) map.get(0)[0]);
+//		return adres;
+//	}
+//
+//	public Rol rolOphalen(String rol) {
+//		return null;
+//	}
+//
+//	public Rol rolToevoegen(String rolNaam) {
+//
+//		return null;
+//	}
+//
+//	private Map<Integer, Object[]> executeQuery(String query, boolean update, Object... kolomWaarden) {
+//		Map<Integer, Object[]> map = new HashMap<Integer, Object[]>();
+//		java.sql.Connection connection = null;
+//		PreparedStatement stmt = null;
+//		ResultSet resultSet = null;
+//		try {
+//			connection = Connection.getDBConnection();
+//			connection.setAutoCommit(false);
+//			stmt = connection.prepareStatement(query);
+//			for (int i = 0; i < kolomWaarden.length; i++) {
+//				stmt.setObject(i + 1, kolomWaarden[i]);
+//			}
+//			if (update) {
+//				stmt.executeUpdate();
+//				connection.commit();
+//			} else {
+//				resultSet = stmt.executeQuery();
+//				ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+//				int columnCount = resultSetMetaData.getColumnCount();
+//				resultSet.last();
+//				int size = resultSet.getRow();
+//				resultSet.beforeFirst();
+//				int counter = 0;
+//
+//				if (size != 0) {
+//					while (resultSet.next()) {
+//						Object[] kolommen = new Object[columnCount];
+//						for (int i = 1; i <= columnCount; i++) {
+//							kolommen[i - 1] = resultSet.getObject(i);
+//						}
+//						map.put(counter, kolommen);
+//						counter++;
+//					}
+//					;
+//
+//				}
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			try {
+//				if (resultSet != null)
+//					resultSet.close();
+//				if (stmt != null)
+//					stmt.close();
+//				if (connection != null)
+//					connection.close();
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		return map;
+//	}
+//	private Login loginOphalen(Login login) {
+//		String loginOphalen = "SELECT loginId FROM Login WHERE username LIKE '%LoginDAOTest%'";
+//		Map<Integer, Object[]> map = executeQuery(loginOphalen, false);
+//		login.setLoginId((int) map.get(0)[0]);
+//		return login;
+//	}
 //	
 //	private final String LOGIN_VERWIJDEREN_OP_USERNAME="";
 //	private final String MEDEWERKER_VERWIJDEREN_OP_VOOR_EN_ACHTERNAAM="";
