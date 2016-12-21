b24feccec4f56eb7aa3c29a2e094862e29522d41
Aime <PEGASUS@PEGASUS>
2016-12-21 17:09:21 +0100

    LoginDAOTest (Vervolg)
    	-testGetLoginIdMetUsername
    	-testGetLoginIdMetOnbestaandeUsername
    	-testGetWachtwoord
    	-testGetWachtwoordMetOnbestaandeUsername
    	-testUpdadeWachtwoord
    	-testUpdateWachtwoordVanOnbestaandeUsername
    	-testUpdateWachtwoordWhere
    	-testUpdateWachtwoordWhereMetOnbestaandeId
    	-testGetActief
    	-testGetNietActiefLogin
    	-testGetActiefMetOnbestaandeId

--- testing/dao/LoginDAOTest.java
+++ testing/dao/LoginDAOTest.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 
 import dao.LoginDao;
 import source.Adres;
+import source.Klant;
 import source.Login;
 import source.Medewerker;
 import source.Persoon;
@@ -33,9 +34,7 @@ public class LoginDAOTest {
 
 	@Before
 	public void initialize() {
-		// adres = adresToevoegen(new Adres("teststraatnaamLoginDAOTest", 170,
-		// "testwoonplaatsLoginDAOTest", 1070, "6"));
-		// rol= rolToevoegen(new Rol(0, "testrolLoginDAOTest"));
+
 		// this is not needed to test login
 		// but useful for medewerkerdao test
 		login = new Login("testusernameLoginDAOTest", "testpasswordLoginDAOTest");
@@ -85,73 +84,195 @@ public class LoginDAOTest {
 	@Test
 	public void testGetLogin() {
 		Login loginVanUitDB = loginToevoegen(login);
-		assertEquals(loginVanUitDB, LoginDao.getLogin(loginVanUitDB.getLoginId())); 
+		assertEquals(loginVanUitDB, LoginDao.getLogin(loginVanUitDB.getLoginId()));
 	}
+
 	@Test
 	public void testGetLoginMetOnbestaandeId() {
-		int onBestaandeLoginId=91119;
-		assertNull(LoginDao.getLogin(onBestaandeLoginId)); 
-	} 
+		int onBestaandeLoginId = 91119;
+		assertNull(LoginDao.getLogin(onBestaandeLoginId));
+	}
+
 	@Test
 	public void testCheckUsername() {
 		Login loginVanUitDB = loginToevoegen(login);
-		LoginDao loginDao= new LoginDao();
-		assertEquals(loginVanUitDB.getLoginId(), loginDao.checkUsername(login.getUsername())); 
+		LoginDao loginDao = new LoginDao();
+		assertEquals(loginVanUitDB.getLoginId(), loginDao.checkUsername(login.getUsername()));
 	}
+
 	@Test
 	public void testCheckUsernameMetOnbestaandeUsername() {
-		LoginDao loginDao= new LoginDao();
-		assertEquals(-1, loginDao.checkUsername(login.getUsername())); 
+		LoginDao loginDao = new LoginDao();
+		assertEquals(-1, loginDao.checkUsername(login.getUsername()));
 	}
-//	ik weet niet wat de bedoeling is van dit methode
-//	public void testGetRoll() {
-//	}
-//
-//	public void testGetRollMetOnbestaandeRoll() {
-//	}
+
+	// ik weet niet wat de bedoeling is van dit methode
+	// public void testGetRoll() {
+	// }
+	//
+	// public void testGetRollMetOnbestaandeRoll() {
+	// }
 	@Test
 	public void testGetLoginIdMetUsername() {
 		Login loginVanUitDB = loginToevoegen(login);
-		assertEquals(loginVanUitDB.getLoginId(), LoginDao.getLoginId(login.getUsername())); ;
+		assertEquals(loginVanUitDB.getLoginId(), LoginDao.getLoginId(login.getUsername()));
+		;
 	}
+
 	@Test
 	public void testGetLoginIdMetOnbestaandeUsername() {
-		assertEquals(0, LoginDao.getLoginId(login.getUsername())); 
+		assertEquals(0, LoginDao.getLoginId(login.getUsername()));
 	}
 
+	@Test
 	public void testGetWachtwoord() {
+		loginToevoegen(login);
+		assertEquals(login.getPassword(), LoginDao.getWachtwoord(login.getUsername()));
+		;
 	}
 
+	@Test
 	public void testGetWachtwoordMetOnbestaandeUsername() {
+		assertEquals(login.getUsername(), LoginDao.getWachtwoord(login.getUsername()));
 	}
 
+	@Test
 	public void testUpdadeWachtwoord() {
+		loginToevoegen(login);
+		// wie de login en logindao heeft gemaakt heeft ervoor gezorgd
+		// de volgende stapen moet door lopen om dit te testen
+		Login loginTmp = new Login();
+		loginTmp.setCurrentUser(login.getUsername());
+		login.setPassword("testUpdadeWachtwoordLoginDAOTest");
+		LoginDao.updateWachtwoord(login.getPassword());
+		assertEquals(login.getPassword(), loginOphalen(login).getPassword());
 	}
 
+	@Test(expected = Exception.class)
+	public void testUpdateWachtwoordVanOnbestaandeUsername() {
+		LoginDao.updateWachtwoord(login.getPassword());
+	}
+
+	@Test
 	public void testUpdateWachtwoordWhere() {
+		Login loginVanUitDB = loginToevoegen(login);
+		String ww = "testUpdadeWachtwoordLoginDAOTest";
+		LoginDao.updateWachtwoordWhere(loginVanUitDB.getLoginId(), ww);
+		loginVanUitDB = loginOphalen(login);
+		assertEquals(ww, loginVanUitDB.getPassword());
+		;
 	}
 
+	@Test(expected = Exception.class)
+	public void testUpdateWachtwoordWhereMetOnbestaandeId() {
+		String ww = "testUpdadeWachtwoordLoginDAOTest";
+		LoginDao.updateWachtwoordWhere(91119, ww);
+	}
+
+	@Test
 	public void testGetActief() {
+		adres = adresToevoegen(new Adres("teststraatnaamLoginDAOTest", 170, "testwoonplaatsLoginDAOTest", 1070, "6"));
+		rol = rolToevoegen(new Rol(0, "testrolLoginDAOTest"));
+		Persoon persoon=new Persoon(
+				"testvoornaamLoginDAOTest", "testachternaamLoginDAOTest",
+				"testemailLoginDAOTest", adres);
+		medewerker=new Medewerker("testvoornaamLoginDAOTest", "testachternaamLoginDAOTest");
+		medewerker.setEmail(persoon.getEmail());
+		medewerker.setAdres(persoon.getAdres());
+		medewerker.setRol(rol);
+		medewerker.setLogin(loginToevoegen(login));
+		medewerker.setActief(true);
+		medewerker=medewerkerOpslaanInDB(medewerker); 
+		assertEquals(1,LoginDao.getActief(medewerker.getLogin().getLoginId()));
+		medewerkerVerwijderen(medewerker);
 	}
-
-	public void testGetActiefMetOnbestaandeid() {
+	@Test
+	public void testGetNietActiefLogin() {
+		adres = adresToevoegen(new Adres("teststraatnaamLoginDAOTest", 170, "testwoonplaatsLoginDAOTest", 1070, "6"));
+		rol = rolToevoegen(new Rol(0, "testrolLoginDAOTest")); 
+		Persoon persoon=new Persoon(
+				"testvoornaamLoginDAOTest", "testachternaamLoginDAOTest",
+				"testemailLoginDAOTest", adres);
+		medewerker=new Medewerker("testvoornaamLoginDAOTest", "testachternaamLoginDAOTest");
+		medewerker.setEmail(persoon.getEmail());
+		medewerker.setAdres(persoon.getAdres());
+		medewerker.setRol(rol);
+		medewerker.setLogin(loginToevoegen(login));
+		medewerker.setActief(false);
+		medewerker=medewerkerOpslaanInDB(medewerker); 
+		assertEquals(0,LoginDao.getActief(medewerker.getLogin().getLoginId())); 
+		medewerkerVerwijderen(medewerker);
+		
 	}
+	@Test
+	public void testGetActiefMetOnbestaandeId() {
+		int onbestaandeId=91119;
+		assertEquals(onbestaandeId,LoginDao.getActief(onbestaandeId));  
+	}
+	private void medewerkerVerwijderen(Medewerker medewerker){
+		String verwijderMedewerker="DELETE FROM Medewerker WHERE persoonId= ?";
+		String verwijderRol="DELETE FROM Rol WHERE rol LIKE '%LoginDAOTest%'";
+		String verwijderPersoon="DELETE FROM Persoon WHERE voornaam LIKE '%LoginDAOTest%'";
+		String verwijderAdres="DELETE FROM Adres WHERE straat LIKE '%LoginDAOTest%'";
+		executeQuery(verwijderMedewerker, true, medewerker.getId());
+		executeQuery(verwijderRol, true);
+		executeQuery(verwijderPersoon, true);
+		executeQuery(verwijderAdres, true); 
+	}
+	private Medewerker medewerkerOpslaanInDB(Medewerker medewerker) {
+		String persoonToevoegenQuery = "INSERT INTO Persoon "
+				+ "(adresId, voornaam, achternaam, email) "
+				+ "VALUES(?,?,?,?)";
+		String persoonZoekenQuery = "SELECT persoonId "
+				+ "FROM Persoon "
+				+ "WHERE voornaam = ?";
 
+		String medewerkerToevoegenQuery = "INSERT INTO Medewerker "
+				+ "(loginId, persoonId, rolId,Actief) "
+				+ "VALUES(?,?,?,?)";
+		String medewerkerZoekenQuery = "SELECT medewerkerId FROM Medewerker "
+				+ "WHERE persoonId = ?"; 
+		executeQuery(persoonToevoegenQuery, true,
+				medewerker.getAdres().getAdresId(),
+				medewerker.getVoornaam(),
+				medewerker.getAchternaam(),
+				medewerker.getEmail()
+				);
+		Map<Integer, Object[]>map=executeQuery(persoonZoekenQuery, false, 
+				medewerker.getVoornaam()
+				);
+		medewerker.setId((int)map.get(0)[0]);
+		executeQuery(medewerkerToevoegenQuery, true,
+				medewerker.getLogin().getLoginId(),
+				medewerker.getId(),
+				medewerker.getRol().getRolId(),
+				medewerker.isActief()?1:0
+				);
+		map=executeQuery(medewerkerZoekenQuery, false, 
+				medewerker.getId()
+				);
+		medewerker.setMedewerkerId((int)map.get(0)[0]); 
+		return medewerker;
+	}
 	public Login loginToevoegen(Login login) {
 		String loginToevoen = "INSERT INTO Login (username, pass) " + "VALUES(?,?)";
 		executeQuery(loginToevoen, true, login.getUsername(), login.getPassword());
 		return loginOphalen(login);
-	}
-
-	public void medewerkerToevoegen() {
-	}
-
-	public void persoonToevoegen() {
-	}
-
+	} 
 	public Rol rolToevoegen(Rol rol) {
-
-		return null;
+		String rolToevoegenQuery = "INSERT INTO Rol "
+				+ "(rol) "
+				+ "VALUES(?)";
+		String medewerkerZoekenQuery = "SELECT rolId FROM Rol "
+				+ "WHERE rol = ?"; 
+		executeQuery(rolToevoegenQuery, true,
+				rol.getRol() 
+				);
+		Map<Integer, Object[]>map=executeQuery(medewerkerZoekenQuery, false, 
+				rol.getRol()
+				);
+		rol.setRolId((int)map.get(0)[0]);  
+		return rol;
 	}
 
 	public Adres adresToevoegen(Adres adres) {
@@ -162,17 +283,7 @@ public class LoginDAOTest {
 		Map<Integer, Object[]> map = executeQuery(AdresZoekenQuery, false, adres.getStraat());
 		adres.setAdresId((int) map.get(0)[0]);
 		return adres;
-	}
-
-	public Rol rolOphalen(String rol) {
-		return null;
-	}
-
-	public Rol rolToevoegen(String rolNaam) {
-
-		return null;
-	}
-
+	} 
 	private Map<Integer, Object[]> executeQuery(String query, boolean update, Object... kolomWaarden) {
 		Map<Integer, Object[]> map = new HashMap<Integer, Object[]>();
 		java.sql.Connection connection = null;
@@ -234,150 +345,5 @@ public class LoginDAOTest {
 		Map<Integer, Object[]> map = executeQuery(loginOphalen, false);
 		login.setLoginId((int) map.get(0)[0]);
 		return login;
-	}
-	// public void testGetUserName(){}
-	// login is totaal anders in klassendiagram
-	// en deze testen zijn niet meer van toepasing
-	// -Aime
-
-	// @SuppressWarnings("unused")
-	// private int userId, fakeUserID, rollId, fakeRollId;
-	// private String username, password, fakeUsername, fakePassword;
-	//
-	// @Before
-	// public void Init() {
-	// // de volgende is de setup van/voor 1 user, namelijk de test user die al
-	// // bestaan in DB
-	// userId = 1;
-	// fakeUserID = 999;
-	// rollId = 1;
-	// fakeRollId = 999;
-	//
-	// username = "test";
-	// password = "test";
-	// fakeUsername = "fakeusername";
-	// fakePassword = "fakepassword";
-	//
-	// }
-	// // ----------------------------------- getUsername Tests
-	//
-	// @Test
-	// public void getUsernameUsingExistingUserId() {
-	// // fail test
-	// // assertEquals(username, LoginDao.getUsername(fakeUserID));
-	// // success test
-	// assertEquals(username, LoginDao.getUsername(userId));
-	// }
-	//
-	// @Test
-	// public void getUsernameUsingNonExistentUserId() {
-	// // fail test
-	// // assertEquals(username, LoginDao.getUsername(userId));
-	// // success test
-	// assertEquals(null, LoginDao.getUsername(fakeUserID));
-	// }
-	//
-	// @Test
-	// public void getUsernameUsingExistingUsername() {
-	// // fail test
-	// // assertEquals(username, LoginDao.getUserName(fakeusername));
-	// // success test
-	// assertEquals(username, LoginDao.getUserName(username));
-	// }
-	//
-	// @Test
-	// public void getUsernameUsingNonExistentUsername() {
-	// // fail test
-	// // assertEquals(null, LoginDao.getUserName(username));
-	// // success test
-	// assertEquals(null, LoginDao.getUserName(fakeUsername));
-	// }
-	//
-	// // ----------------------------------- getPassword Tests
-	//
-	// @Test
-	// public void getPasswordUsingExistingUserId() {
-	// // fail test
-	// // assertEquals(password, LoginDao.getPassword(fakeUserID));
-	// // success test
-	// assertEquals(password, LoginDao.getPassword(userId));
-	// }
-	//
-	// @Test
-	// public void getPasswordUsingNonExistentUserId() {
-	// // fail test
-	// // assertEquals(null, LoginDao.getPassword(userId));
-	// // success test
-	// assertEquals(null, LoginDao.getPassword(fakeUserID));
-	// }
-	//
-	// // ----------------------------------- getRoll Tests
-	// @Test
-	// public void getRollUsingExistingUsername() {
-	// // fail test
-	// // assertEquals(rollId, LoginDao.getRoll(fakeUsername));
-	// // success test
-	// assertEquals(rollId, LoginDao.getRoll(username));
-	// }
-	//
-	// @Test
-	// public void getRollUsingNonExistentUsername() {
-	// // fail test
-	// // assertEquals(null, LoginDao.getRoll(username));
-	// // success test
-	// assertEquals(0, LoginDao.getRoll(fakeUsername));
-	// }
-	//
-	// // ----------------------------------- getUserId Tests
-	// @Test
-	// public void getUserIdUsingExistingUsernameAndCorrectPassword() {
-	// // fail tests
-	// // assertEquals(userId, LoginDao.userId(fakeUsername, fakePassword));
-	// // assertEquals(userId, LoginDao.userId(username, fakePassword));
-	// // assertEquals(userId, LoginDao.userId(fakeUsername, password));
-	// // success test
-	// assertEquals(userId, LoginDao.userId(username, password));
-	// }
-	//
-	// @Test
-	// public void getUserIdUsingNonExistentUsernameAndWrongPassword() {
-	// // fail test
-	// // assertEquals(0, LoginDao.userId(username, password));
-	// // success test
-	// assertEquals(0, LoginDao.userId(fakeUsername, fakePassword));
-	// }
-	//
-	// @Test
-	// public void getUserIdUsingExistingUsernameAndWrongPassword() {
-	// // fail test
-	// // assertEquals(null, LoginDao.userId(username, password));
-	// // success test
-	// assertEquals(0, LoginDao.userId(username, fakePassword));
-	// }
-	//
-	// @Test
-	// public void getUserIdUsingNonExistentUsernameAndCorrectPassword() {
-	// // fail test
-	// // assertEquals(0, LoginDao.userId(username, password));
-	// // success test
-	// assertEquals(0, LoginDao.userId(fakeUsername, password));
-	// }
-	//
-	// // ----------------------------------- getWachtwoord Tests
-	// @Test
-	// public void getWachtwoordUsingExistingUsername() {
-	// // fail test
-	// // assertEquals(password, LoginDao.getWachtwoord(fakeUsername));
-	// // success test
-	// assertEquals(password, LoginDao.getWachtwoord(username));
-	// }
-	//
-	// @Test
-	// public void getWachtwoordNonExistentUsername() {
-	// // fail test
-	// // assertEquals(null, LoginDao.getWachtwoord(username));
-	// // success test
-	// assertEquals(null, LoginDao.getWachtwoord(fakeUsername));
-	// }
-
+	} 
 }
