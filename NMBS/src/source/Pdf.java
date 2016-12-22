a53a484697522439222f1cd1d79ac9bd28753852
MarwaneEL <marwane.el-aissati@student.ehb.Be>
2016-12-22 17:19:59 +0100

    edit classe PDF

--- NMBS/src/source/Pdf.java
+++ NMBS/src/source/Pdf.java
@@ -15,10 +15,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-
+import dao.KlantDAO;
+import dao.StationDAO;
 import dao.TicketDao;
 
 
@@ -26,8 +28,7 @@ public class Pdf {
 
 	public void TicketGenerator(int id) {
 		
-		//TicketDao tDao = new TicketDao();
-		Ticket ticket = TicketDao.getTicket(id);
+		Ticket ticket = TicketDao.getTicket(2);
 		
 		Document document = new Document();
 		Rectangle rectangle = new Rectangle(PageSize.A4);
@@ -50,7 +51,8 @@ public class Pdf {
 		    
 			
 		    document.add(image);
-			document.add(new Paragraph("Ticket nummer:  " + id + "\n\n",fonts[0]));
+			
+		    document.add(new Paragraph("Ticket nummer:  " + ticket.getAankoopId() + "\n\n",fonts[0]));
 			document.add(new Paragraph("GELDIG VOOR EEN REIS\n",fonts[1]));
 			if(ticket.getTerugDatum() == null){
 				document.add(new Paragraph("HEENREIS",fonts[1]));
@@ -84,7 +86,7 @@ public class Pdf {
 			document.add(new Paragraph("Reizigers: " + ticket.getAantal(), fonts[1]));
 			String imageUrl2 = "http://optional.is/required/wp-content/uploads/2009/06/barcode-pdf417.png";
 			Image image2 = Image.getInstance(new URL(imageUrl2));
-			 image2.scaleAbsolute(200f, 80f);
+			image2.scaleAbsolute(200f, 80f);
 		    document.add(image2);
 
 			document.close();
@@ -97,88 +99,11 @@ public class Pdf {
 		        File myFile = new File("../NMBS-java/ticket.pdf");
 		        Desktop.getDesktop().open(myFile);
 		    } catch (IOException ex) {
-		        // no application registered for PDFs
+		       //no application registered for PDFs
 		    }
 		}
-		
-		
 	}
-	
-	public void AboGenerator(Abonnement abo) {
-		//code abo
-	}
-	/*
-	public static void main(String[] args) {
-		Abonnement abo = new Abonnement(15264, 12354, 1562, 2, "Bruxelles-Nord", "Bruxelles-Zuid", 50.0, null, 0.0, true);
-		Document document = new Document();
-		Rectangle rectangle = new Rectangle(PageSize.A4);
-		document.setPageSize(rectangle);
 		
-		Font[] fonts = {
-				new Font(Font.FontFamily.COURIER,12,Font.BOLD),
-				new Font(Font.FontFamily.COURIER,10),
-				new Font(Font.FontFamily.COURIER,13),
-				new Font(Font.FontFamily.COURIER,18)
-		};
-	
-		try {
-			PdfWriter.getInstance(document, new FileOutputStream("abonnement.pdf"));
-			
-			document.open();
-			
-			String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/SNCB_logo.svg/1000px-SNCB_logo.svg.png";
-			Image image = Image.getInstance(new URL(imageUrl));
-		    image.scaleAbsolute(50f, 30f);
-		    document.add(image);
-		    
-			document.add(new Paragraph(abo.getAbonnementId() + "",fonts[3]));
-			document.add(new Paragraph(2 + "e klas\n\n",fonts[2]));
-			/*String depStation = ticket.getDepZone();
-			document.add(new Paragraph("Van station:    " + depStation.toUpperCase() + "\n",fonts[0]));
-			String arrStation = ticket.getArrZone();
-			document.add(new Paragraph("Tot station:    " + arrStation.toUpperCase() + "\n\n",fonts[0]));
-			
-			
-			DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
-			Date datum = ticket.getHeenDatum();    
-			String heenDatum = df.format(datum);
-			document.add(new Paragraph("Heen op:        " + heenDatum + "\n",fonts[0]));
-			if(ticket.getTerugDatum() != null){
-				datum = ticket.getTerugDatum();   
-				String terugdatum = df.format(datum);
-				document.add(new Paragraph("Terug op:       " + terugdatum,fonts[0]));
-			}
-			
-			
-			DateFormat dfh = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-			datum = Calendar.getInstance().getTime(); 
-			String reportDate = dfh.format(datum);
-			document.add(new Paragraph("\n\n" +  "Verkoopdatum:       " + reportDate + "\n",fonts[1]));
-			
-			document.add(new Paragraph("Reizigers: " + ticket.getAantal(), fonts[1]));
-			String imageUrl2 = "http://optional.is/required/wp-content/uploads/2009/06/barcode-pdf417.png";
-			Image image2 = Image.getInstance(new URL(imageUrl2));
-			 image2.scaleAbsolute(200f, 80f);
-		    document.add(image2);
-
-			document.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 		
-		
-		if (Desktop.isDesktopSupported()) {
-		    try {
-		        File myFile = new File("../NMBS-java/abonnement.pdf");
-		        Desktop.getDesktop().open(myFile);
-		    } catch (IOException ex) {
-		        // no application registered for PDFs
-		    }
-		}
-		
-		
-	}
-	*/
-	
-	
-	
 }
+	
+	
