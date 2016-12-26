b12485a8c88a99aff65af7369c08adfb6c0060ba
wehb <author@example.com>
2016-12-26 21:49:23 +0100

    OS-check goed gefixt nu.

--- NMBS/src/dao/CheckIfConnected.java
+++ NMBS/src/dao/CheckIfConnected.java
@@ -4,28 +4,44 @@ import java.io.IOException;
 
 public class CheckIfConnected {
 	private static String os = System.getProperty("os.name").toLowerCase();
+	private static boolean windows = false;
+	private static boolean linux = false;
+	private static boolean mac = false;
 	
 	public static boolean checkIfConnected() {
 		
 		//Gebaseerd op http://stackoverflow.com/questions/9922543/why-does-inetaddress-isreachable-return-false-when-i-can-ping-the-ip-address
 		Process p1;
 		try {
-			if (os.substring(0, 7).equals("windows") || os.equals("linux"))
+			//ik doe de check op deze manier om een out of bounds exception te verkomen bij kortere namen (bv. "windows 10" vs "linux")
+			if (os.length() > 6) {
+				if (os.substring(0, 7).equals("windows")) {
+					windows = true;
+				}
+			}
+			if (os.length() > 5) {//voorkom out of bounds exception bij kortere naam
+				if (os.substring(0, 6).equals("linux")) {
+					linux = true;
+				}
+			}
+			if (os.length() > 2) {//voorkom out of bounds exception bij kortere naam
+				if (os.substring(0, 3).equals("mac")) {
+					mac = true;
+				}
+			}
+			if (windows || linux)
 			{
 				p1 = java.lang.Runtime.getRuntime().exec("ping -w 1 www.google.com");
 				//"-w 1" to make it not unbearably slow on Linux (tested in Ubuntu 16.04.1) when connected to the Internet
 				//and slightly faster on Windows, too
-				System.out.println("windows or linux?: " + os);
 			}
 			else
 			{
-				if (os.substring(0, 3).equals("mac")) {
+				if (mac) {
 					p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
-					System.out.println("is this mac?: " + os);
 				}
 				else {
 					p1 = java.lang.Runtime.getRuntime().exec("ping www.google.com");
-					System.out.println("not windows, linux or linux?: " + os);
 				}
 			}
 			int returnVal = p1.waitFor();
@@ -40,28 +56,4 @@ public class CheckIfConnected {
 		}
 		return false;
 	}
-	
-	 private static String OS = System.getProperty("os.name").toLowerCase();
-
-	    public static void detectOS() {
-	        if (isWindows()) {
-	        	os = "windows";
-	        } else if (isMac()) {
-	        	os = "mac";
-	        } else if (isUnix()) {
-	        	os = "linux"; 
-	        } 
-	    }
-
-	    private static boolean isWindows() {
-	        return (OS.indexOf("win") >= 0);
-	    }
-
-	    private static boolean isMac() {
-	        return (OS.indexOf("mac") >= 0);
-	    }
-
-	    private static boolean isUnix() {
-	        return (OS.indexOf("nux") >= 0);
-	    }
 }
