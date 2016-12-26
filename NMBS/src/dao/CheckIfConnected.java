3155fa66dc679a2a45f484646da34aacc26e5629
wehb <author@example.com>
2016-12-26 21:07:54 +0100

    CheckIfConnected goed gefixt (moet wel nog getest worden op mac)

--- NMBS/src/dao/CheckIfConnected.java
+++ NMBS/src/dao/CheckIfConnected.java
@@ -3,23 +3,30 @@ package dao;
 import java.io.IOException;
 
 public class CheckIfConnected {
-	private static String os;
+	private static String os = System.getProperty("os.name").toLowerCase();
 	
 	public static boolean checkIfConnected() {
 		
 		//Gebaseerd op http://stackoverflow.com/questions/9922543/why-does-inetaddress-isreachable-return-false-when-i-can-ping-the-ip-address
 		Process p1;
 		try {
-			if (os == "windows" || os == "linux") 
+			if (os.substring(0, 7).equals("windows") || os.equals("linux"))
 			{
 				p1 = java.lang.Runtime.getRuntime().exec("ping -w 1 www.google.com");
 				//"-w 1" to make it not unbearably slow on Linux (tested in Ubuntu 16.04.1) when connected to the Internet
 				//and slightly faster on Windows, too
+				System.out.println("windows or linux?: " + os);
 			}
 			else
 			{
-				//p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
-				p1 = java.lang.Runtime.getRuntime().exec("ping www.google.com");
+				if (os.substring(0, 3).equals("mac")) {
+					p1 = java.lang.Runtime.getRuntime().exec("ping -c 1 www.google.com");
+					System.out.println("is this mac?: " + os);
+				}
+				else {
+					p1 = java.lang.Runtime.getRuntime().exec("ping www.google.com");
+					System.out.println("not windows, linux or linux?: " + os);
+				}
 			}
 			int returnVal = p1.waitFor();
 			boolean reachable = (returnVal==0);
