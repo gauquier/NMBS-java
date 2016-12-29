66a6d953b817395660788b5ce8f472706320f476
MarwaneEL <marwane.el-aissati@student.ehb.Be>
2016-12-29 15:06:17 +0100

    commit laatste versie van verlorenvoorwerpZoeken

--- NMBS/src/gui/VerlorenVoorwerpenZoekenGui.java
+++ NMBS/src/gui/VerlorenVoorwerpenZoekenGui.java
@@ -1,5 +1,6 @@
 
 package gui;
+
 import javax.swing.JPanel;
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
@@ -29,115 +30,101 @@ import javax.swing.JOptionPane;
 import java.awt.SystemColor;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
-
 public class VerlorenVoorwerpenZoekenGui extends JPanel {
+	
 	private JComboBox stationLijst;
 	private StationDAO stationDAO = new StationDAO();
 	private JButton btnBewerken;
 	private JList<VerlorenVoorwerp> list;
-	
+
 	private VerlorenVoorwerpDAO verlorenVoorwerpDAO = new VerlorenVoorwerpDAO();
 	private ArrayList<VerlorenVoorwerp> arrayLijst;
 	private ArrayList<Object> objecten;
 	private DefaultListModel<VerlorenVoorwerp> dlm;
-	
+
 	String navigation;
-	
-	
-	
+
 	public VerlorenVoorwerpenZoekenGui() {
 		setBackground(UIManager.getColor("CheckBoxMenuItem.selectionBackground"));
-		
+
 		JLabel lblStation = new JLabel("Station");
 		lblStation.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblStation.setForeground(Color.WHITE);
-		
+
 		JLabel lblVerlorenVoorwerpenZoeken = DefaultComponentFactory.getInstance().createTitle("Verloren voorwerpen zoeken");
 		lblVerlorenVoorwerpenZoeken.setForeground(Color.WHITE);
 		lblVerlorenVoorwerpenZoeken.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		
+
 		stationLijst = new JComboBox();
 		ArrayList<Station> lijst = stationDAO.getAll();
 		for (Station station : lijst) {
 			stationLijst.addItem(station);
 		}
-		
-		
-		stationLijst.setSelectedIndex(0);//veranderen naar current user station id
-		arrayLijst= new ArrayList<VerlorenVoorwerp>();
-		arrayLijst = verlorenVoorwerpDAO.getVerlorenVoorwerpByStation(1);//veranderen naar current user station id
+
+		stationLijst.setSelectedIndex(0);
+		arrayLijst = new ArrayList<VerlorenVoorwerp>();
+		arrayLijst = verlorenVoorwerpDAO.getVerlorenVoorwerpByStation(1);
 		dlm = new DefaultListModel<VerlorenVoorwerp>();
-		for(VerlorenVoorwerp v : arrayLijst){
+		
+		for (VerlorenVoorwerp v : arrayLijst) {
 			dlm.addElement(v);
 		}
-		
+
 		list = new JList<VerlorenVoorwerp>(dlm);
 		list.setBackground(SystemColor.menu);
-		
+
 		stationLijst.addItemListener(new ItemListener() {
-			
+
 			@Override
 			public void itemStateChanged(ItemEvent e) {
-				
-				arrayLijst.clear();
-				dlm.removeAllElements();
-				
-				//arrayLijst= new ArrayList<VerlorenVoorwerp>();
-				arrayLijst = verlorenVoorwerpDAO.getVerlorenVoorwerpByStation(stationDAO.checkStation(stationLijst.getSelectedItem().toString()));//veranderen naar current user station id
-				System.out.println(stationLijst.getSelectedItem());
-				
-				for(VerlorenVoorwerp v : arrayLijst){
-					dlm.addElement(v);
+				ArrayList<VerlorenVoorwerp> t = new ArrayList<VerlorenVoorwerp>();
+
+				t = verlorenVoorwerpDAO.getVerlorenVoorwerpByStation(stationDAO.checkStation(stationLijst.getSelectedItem().toString()));
+
+				dlm.clear();
+
+				for (VerlorenVoorwerp m : t) {
+					dlm.addElement(m);
 				}
-				
-				list = new JList<VerlorenVoorwerp>(dlm);
-				
+
+				list.setModel(dlm);
 			}
 		});
-		
+
 		btnBewerken = new JButton("gevonden");
 		btnBewerken.addActionListener(new MenuItemHandler());
-		
-		
 
 		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(42)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
+				.createSequentialGroup().addGap(42)
+				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addComponent(lblVerlorenVoorwerpenZoeken, GroupLayout.DEFAULT_SIZE, 724, Short.MAX_VALUE)
-						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-							.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-								.addComponent(lblStation)
-								.addGap(32)
-								.addComponent(stationLijst, GroupLayout.PREFERRED_SIZE, 302, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(btnBewerken, GroupLayout.PREFERRED_SIZE, 238, GroupLayout.PREFERRED_SIZE))
-							.addComponent(list, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 701, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap())
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(33)
-					.addComponent(lblVerlorenVoorwerpenZoeken)
-					.addGap(27)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(stationLijst, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblStation)
-						.addComponent(btnBewerken))
-					.addGap(30)
-					.addComponent(list, GroupLayout.PREFERRED_SIZE, 237, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(334, Short.MAX_VALUE))
-		);
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false).addGroup(Alignment.LEADING,
+								groupLayout.createSequentialGroup().addComponent(lblStation).addGap(32)
+										.addComponent(stationLijst, GroupLayout.PREFERRED_SIZE, 302,
+												GroupLayout.PREFERRED_SIZE)
+										.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE,
+												Short.MAX_VALUE)
+										.addComponent(btnBewerken, GroupLayout.PREFERRED_SIZE, 238,
+												GroupLayout.PREFERRED_SIZE))
+								.addComponent(list, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 701,
+										GroupLayout.PREFERRED_SIZE)))
+				.addContainerGap()));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addGap(33).addComponent(lblVerlorenVoorwerpenZoeken)
+						.addGap(27)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(stationLijst, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblStation).addComponent(btnBewerken))
+						.addGap(30).addComponent(list, GroupLayout.PREFERRED_SIZE, 237, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(334, Short.MAX_VALUE)));
 		setLayout(groupLayout);
 	}
-	public void close()
-	{
+
+	public void close() {
 		this.setVisible(false);
 	}
-	
 
 	private class MenuItemHandler implements ActionListener {
 
@@ -149,11 +136,11 @@ public class VerlorenVoorwerpenZoekenGui extends JPanel {
 				if (list.getSelectedValue() == null) {
 					JOptionPane.showMessageDialog(new JFrame(), "Geen verloren voorwerp aangeduid.");
 				} else {
-					 
+
 					verlorenVoorwerpDAO.verlorenVoowerpUpdate(verlorenVoorwerpDAO.getId(list.getSelectedValue()));
-					JOptionPane.showMessageDialog(new JFrame(), "Verloren verwijdert");
+					JOptionPane.showMessageDialog(new JFrame(), "Verloren voorwerp gevonden");
 					AdminGui.setHuidigeKeuze(new VerlorenVoorwerpenZoekenGui());
-						
+
 				}
 			}
 
